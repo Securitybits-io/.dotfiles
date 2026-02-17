cc770a224d67a3d4462c93c57815280e67c35058
Christoffer Claesson <christoffer.claesson@outlook.com>
2026-02-17 16:07:58 +0000

    added lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,31 +1,3 @@
--- Line Numbers (which we already discussed)
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-
--- Indentation (Standard 4-space indent)
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set smartindent")
-
--- Search Behavior
-vim.opt.ignorecase = true     -- Ignore case when searching...
-vim.opt.smartcase = true      -- ...unless you use a capital letter
-vim.opt.hlsearch = false      -- Don't keep previous search highlighted (personal preference)
-
--- Interface Improvements
-vim.opt.scrolloff = 8         -- Keep 8 lines above/below cursor (stops it hitting the edge)
-vim.opt.mouse = 'a'           -- Enable mouse support (clicking, scrolling)
-vim.opt.cursorline = true     -- Highlight the line the cursor is on
-vim.opt.wrap = false          -- Don't wrap long lines (better for code)
-
--- Utility
-vim.cmd("set undofile")
-
--- Set the Leader
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -42,5 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 local opts = {}
 local plugins = {}
 
+require("vim-options")
 require("lazy").setup("plugins")
 
