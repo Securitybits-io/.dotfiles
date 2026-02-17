5b7ad3ba9e3467ff4899965abf665e71e098d62b
Christoffer Claesson <christoffer.claesson@outlook.com>
2026-02-17 17:40:30 +0100

    show .files in nvimm tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,18 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons", 
-  },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>" )
-  end
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", 
+    },
+
+    config = function()
+      vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>" )
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = { visible = true },
+        },
+      })
+    end
 }
