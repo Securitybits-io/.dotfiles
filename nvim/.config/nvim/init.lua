f1cb856610a9e4b41909b8496a5b197584671edc
Christoffer Claesson <christoffer.claesson@outlook.com>
2026-02-17 14:51:50 +0000

    added fonts etc.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,27 +8,21 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set smartindent")
---
--- -- Search Behavior
--- vim.opt.ignorecase = true     -- Ignore case when searching...
--- vim.opt.smartcase = true      -- ...unless you use a capital letter
--- vim.opt.hlsearch = false      -- Don't keep previous search highlighted (personal preference)
---
--- -- Interface Improvements
--- vim.opt.termguicolors = true  -- Enable 24-bit RGB colors
+
+-- Search Behavior
+vim.opt.ignorecase = true     -- Ignore case when searching...
+vim.opt.smartcase = true      -- ...unless you use a capital letter
+vim.opt.hlsearch = false      -- Don't keep previous search highlighted (personal preference)
+
+-- Interface Improvements
 vim.opt.scrolloff = 8         -- Keep 8 lines above/below cursor (stops it hitting the edge)
--- vim.opt.mouse = 'a'           -- Enable mouse support (clicking, scrolling)
--- vim.opt.cursorline = true     -- Highlight the line the cursor is on
--- vim.opt.wrap = false          -- Don't wrap long lines (better for code)
---
--- -- Utility
--- vim.opt.clipboard = "unnamedplus" -- Use system clipboard (copy/paste to other apps)
+vim.opt.mouse = 'a'           -- Enable mouse support (clicking, scrolling)
+vim.opt.cursorline = true     -- Highlight the line the cursor is on
+vim.opt.wrap = false          -- Don't wrap long lines (better for code)
+
+-- Utility
 vim.cmd("set undofile")
---
--- -- Disable the Neovim right-click popup menu
--- vim.cmd([[aunmenu PopUp]])
--- vim.opt.mousemodel = "extend"
---
+
 -- Set the Leader
 vim.g.mapleader = " "
 
@@ -46,17 +40,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } }, 
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
-}
+local plugins = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left" )
+
 
 local config = require("nvim-treesitter.config")
 config.setup({
